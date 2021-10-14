6f1f570f29f1db3b8cc9383680d1db0b2f41f80f
barton26 <barton2526@gmail.com>
2021-10-14 18:14:58 -0400

    Add copyright header, clean up includes

--- src/test/script_tests.cpp
+++ src/test/script_tests.cpp
@@ -1,18 +1,20 @@
+// Copyright (c) 2011-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <vector>
 #include <sstream>
 #include <util/strencodings.h>
-#include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/algorithm/string/split.hpp>
-#include <boost/preprocessor/stringize.hpp>
 #include <boost/test/unit_test.hpp>
 
-#include "main.h"
-#include "wallet/wallet.h"
+#include <main.h>
+#include <wallet/wallet.h>
 
-#include "test/data/script_valid.json.h"
-#include "test/data/script_invalid.json.h"
+#include <test/data/script_valid.json.h>
+#include <test/data/script_invalid.json.h>
 
 #include <cstdint>
 #include <univalue.h>
